beb495d74f1a4f1c882c2619ebd8a1dbeb61d749
StanislavKhabibullin <doctor_haus77@mail.ru>
2020-06-16 21:21:53 +0300

    1.5. Поля объекта [#296296]

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+public class Cat {
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println("Cat's name is - " + this.name);
+        System.out.println("Cat's food is - " + this.food);
+    }
+
+    public void eat(String meat){
+        this.food = meat;
+    }
+    public void giveNick(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Cat gav = new Cat();
+        gav.giveNick("GAV");
+        gav.eat("Kotleta");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
+    }
+}
