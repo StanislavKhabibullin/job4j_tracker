30b3b6f1f56cfdccec9515f152200c6078ba0fc2
StanislavKhabibullin <doctor_haus77@mail.ru>
2020-07-01 13:57:53 +0300

    4.2. Статические методы. [#296259]

--- src/main/java/ru/job4j/tracker/StartUi.java
+++ src/main/java/ru/job4j/tracker/StartUi.java
@@ -2,6 +2,67 @@ package ru.job4j.tracker;
 
 
 public class StartUi {
+    public static void createItem(Input input, Tracker tracker) {
+        String inp = input.askStr("Input request");
+        Item next = new Item(inp);
+        tracker.add(next);
+    }
+
+    public static void showItems(Tracker tracker) {
+        Item[] mas = tracker.findAll();
+        for (int i = 0; i < mas.length; i++) {
+            System.out.println("id: " + mas[i].getId() +
+                    " name: " + mas[i].getName());
+        }
+    }
+
+    public static void editItem(Input input, Tracker tracker) {
+        int promt = input.askInt("Input id elements you want to edit: ");
+        String inp = input.askStr("Input the new request");
+        Item zamena = new Item(inp);
+        if (tracker.replace(promt, zamena)) {
+            System.out.println("Replace succes");
+        } else {
+            System.out.println("Error no such id");
+        }
+    }
+
+    public static void deleteItem(Input input, Tracker tracker) {
+        int promt = input.askInt("Input id elements you want to delete: ");
+        if (tracker.delete(promt)) {
+            System.out.println("Delete succes");
+        } else {
+            System.out.println("Error no such id");
+        }
+    }
+
+    public static void findByItem(Input input, Tracker tracker) {
+        int promt = input.askInt("Input id elements you want to find: ");
+        Item temp = tracker.findById(promt);
+        if (temp != null) {
+            System.out.println("id: " + temp.getId() +
+                    " name: " + temp.getName());
+        } else {
+            System.out.println("No such id");
+        }
+    }
+
+    public static void findByName(Input input, Tracker tracker) {
+        String inp = input.askStr("Input the name of request you want to find");
+        Item[] temp = tracker.findByName(inp);
+        if (temp.length > 0) {
+            for (int i = 0; i < temp.length; i++) {
+                System.out.println("id: " + temp[i].getId() +
+                        " name: " + temp[i].getName());
+            }
+        } else {
+            System.out.println("No such requests");
+        }
+    }
+
+    public static void byBy() {
+        System.out.println("See you soon!");
+    }
 
     public void init(Input input, Tracker tracker) {
         boolean run = true;
@@ -9,58 +70,24 @@ public class StartUi {
             this.showMenu();
             int select = input.askInt("INPUT YOUR CHOICE");
             if (select == 0) {
-                String inp = input.askStr("Input request");
-                Item next = new Item(inp);
-                tracker.add(next);
+                StartUi.createItem(input, tracker);
             } else if (select == 1) {
-                Item[] mas = tracker.findAll();
-                for (int i = 0; i < mas.length; i++) {
-                    System.out.println("id: " + mas[i].getId() +
-                            " name: " + mas[i].getName());
-                }
+                StartUi.showItems(tracker);
             }
             else if (select == 2) {
-                int promt = input.askInt("Input id elements you want to edit: ");
-                    String inp = input.askStr("Input the new request");
-                Item zamena = new Item(inp);
-                    if (tracker.replace(promt, zamena)) {
-                    System.out.println("Replace succes");
-                } else {
-                    System.out.println("Error no such id");
-                }
+                StartUi.editItem(input, tracker);
             }
             else if (select == 3) {
-                int promt = input.askInt("Input id elements you want to delete: ");
-                if (tracker.delete(promt)) {
-                    System.out.println("Delete succes");
-                } else {
-                    System.out.println("Error no such id");
-                }
+                StartUi.deleteItem(input, tracker);
             }
             else if (select == 4) {
-                int promt = input.askInt("Input id elements you want to find: ");
-                Item temp = tracker.findById(promt);
-                if (temp != null) {
-                    System.out.println("id: " + temp.getId() +
-                            " name: " + temp.getName());
-                } else {
-                    System.out.println("No such id");
-                }
+                StartUi.findByItem(input, tracker);
             }
             else if (select == 5) {
-                String inp = input.askStr("Input the name of request you want to find");
-                Item[] temp = tracker.findByName(inp);
-                if (temp.length > 0) {
-                    for (int i = 0; i < temp.length; i++) {
-                        System.out.println("id: " + temp[i].getId() +
-                                " name: " + temp[i].getName());
-                    }
-                } else {
-                    System.out.println("No such requests");
-                }
+                StartUi.findByName(input, tracker);
             }
             else if (select == 6) {
-                System.out.println("See you soon!");
+                StartUi.byBy();
                 run = false;
             }
 
