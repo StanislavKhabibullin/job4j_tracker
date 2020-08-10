a7c3c464e98ef7409fe1afc24d391460200f21d8
StanislavKhabibullin <doctor_haus77@mail.ru>
2020-08-10 17:52:47 +0300

    1. Телефонный справочник на базе ArrayList [#296231]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,26 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList();
+
+    public void add(Person person){
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> rezult = new ArrayList<>();
+        int i = 0;
+        for (Person value:persons
+             ) {
+            if (value.getName().contains(key) ||
+                    value.getSurname().contains(key) ||
+                    value.getAddress().contains(key) ||
+                    value.getPhone().contains(key)) {
+                rezult.add(value);
+            }
+        }
+        return rezult;
+    }
+}
---
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import static org.junit.Assert.*;
+
+public class PhoneDictionaryTest {
+
+    @Test
+    public void findByName() {
+        PhoneDictionary phone = new PhoneDictionary();
+        phone.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> rezult = phone.find("Pet");
+        assertThat(rezult.get(0).getSurname(), is("Arsentev"));
+
+    }
+}
