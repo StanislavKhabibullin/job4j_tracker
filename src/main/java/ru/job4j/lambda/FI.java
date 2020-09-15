eb61ecb2cdba2eb5336ee01188cc44eab7f5e44c
StanislavKhabibullin <66030700+StanislavKhabibullin@users.noreply.github.com>
2020-09-15 17:32:40 +0300

    2.2. Лямбда блок [#296086]

--- src/main/java/ru/job4j/lambda/FI.java
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -8,9 +8,9 @@ import java.util.List;
 public class FI {
     public static void main(String[] args) {
         Attachment[] atts = {
-                new Attachment("image 1", 20),
-                new Attachment("image 3", 120),
-                new Attachment("image 2", 23)
+                new Attachment("image 101", 20),
+                new Attachment("image 30", 120),
+                new Attachment("image 20", 23)
         };
         Comparator<Attachment> comparator = new Comparator<Attachment>() {
             @Override
@@ -18,6 +18,27 @@ public class FI {
                 return o1.getName().compareTo(o2.getName());
             }
         };
+
+        Comparator<Attachment> comparatorName = (left, right) -> {
+           return Integer.valueOf(left.getName().length()).compareTo(Integer.valueOf(right.getName().length()));
+        };
+
+        Comparator<Attachment> cmpDescSize = (left, right) -> {
+            System.out.println("compare - " + left.getName() + " : " + right.getName());
+            return right.getName().length() - left.getName().length();
+        };
+
+        Arrays.sort(atts, comparatorName);
+        for (Attachment schet:atts
+        ) {
+            System.out.println("Сортироовка через лябда выражение - " + schet);
+        }
+
+        Arrays.sort(atts, cmpDescSize);
+        for (Attachment schet:atts
+        ) {
+            System.out.println("Сортироовка через лябда выражение down  " + schet);
+        }
 
         Arrays.sort(atts, comparator);
         for (Attachment schet:atts
