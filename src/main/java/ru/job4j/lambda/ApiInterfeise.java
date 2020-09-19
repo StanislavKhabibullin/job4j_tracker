e82fa21054995f13aef2768fc790c879ce011f2a
StanislavKhabibullin <66030700+StanislavKhabibullin@users.noreply.github.com>
2020-09-19 13:56:06 +0300

    0.1. Отладка лямбда в IDEA [#296099]

--- src/main/java/ru/job4j/lambda/ApiInterfeise.java
+++ src/main/java/ru/job4j/lambda/ApiInterfeise.java
@@ -7,7 +7,9 @@ import java.util.stream.Collectors;
 public class ApiInterfeise {
     public static void main(String[] args) {
         List<Integer> nouns = List.of(-1, 2, 3, 2, -25, 56, 77, 33);
-        List<Integer> plusNouns = nouns.stream().filter(noun -> noun > 0).collect(Collectors.toList());
+        List<Integer> plusNouns = nouns.stream()
+                .filter(noun -> noun > 0)
+                .collect(Collectors.toList());
         System.out.println(plusNouns);
 
     }
