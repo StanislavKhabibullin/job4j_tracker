beade3edcecc50d9b02defa4eeb2b23deb129824
StanislavKhabibullin <66030700+StanislavKhabibullin@users.noreply.github.com>
2020-10-13 22:09:07 +0300

    3. Экзаменацинная задача Stream API

--- src/main/java/ru/job4j/api/Job4jStream.java
+++ src/main/java/ru/job4j/api/Job4jStream.java
@@ -1,29 +1,20 @@
 package ru.job4j.api;
 
+import javax.sql.rowset.Predicate;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collector;
 import java.util.stream.Stream;
 
-public interface Job4jStream extends Stream {
+public class Job4jStream {
 
     // добавляется лишний map чтобы привести Object к требуемому типу в коде
     static Stream<Object> of(Object e1, Object e2, Object e3, Object e4) {
         List<Object> objects = List.of(e1, e2, e3, e4);
         return objects.stream();
     }
-
-    @Override
-    Stream filter(Predicate predicate);
-
-    @Override
-    Stream map(Function mapper);
-
-    @Override
-    Object collect(Collector collector);
 }
