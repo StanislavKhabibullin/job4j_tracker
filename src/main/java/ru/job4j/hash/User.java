1e382022af4e81411a466502d091be615ad3b511
StanislavKhabibullin <66030700+StanislavKhabibullin@users.noreply.github.com>
2021-01-02 07:14:01 +0300

    2. Без переопределения equals и hashCode [#385714]

--- src/main/java/ru/job4j/hash/User.java
+++ src/main/java/ru/job4j/hash/User.java
@@ -1,12 +1,13 @@
 package ru.job4j.hash;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
     private String name;
     private int children;
     private Calendar birthday;
 
+    @SuppressWarnings("checkstyle:EmptyLineSeparator")
     public String getName() {
         return name;
     }
@@ -29,5 +30,81 @@ public class User {
 
     public void setBirthday(Calendar birthday) {
         this.birthday = birthday;
+    }
+
+    @SuppressWarnings("checkstyle:EmptyLineSeparator")
+    public static void main(String[] args) {
+        User user1 = new User();
+        User user2 = new User();
+        user1.setName("Fill");
+        user1.setChildren(2);
+        user1.setBirthday(new GregorianCalendar(1980, Calendar.JANUARY, 12));
+        user2.setName("Fillius");
+        user2.setChildren(1);
+        user2.setBirthday(new GregorianCalendar(1982, Calendar.MAY, 2));
+        Map<User, Object> map = new Map<User, Object>() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean containsKey(Object key) {
+                return false;
+            }
+
+            @Override
+            public boolean containsValue(Object value) {
+                return false;
+            }
+
+            @Override
+            public Object get(Object key) {
+                return null;
+            }
+
+            @Override
+            public Object put(User key, Object value) {
+                return null;
+            }
+
+            @Override
+            public Object remove(Object key) {
+                return null;
+            }
+
+            @Override
+            public void putAll(Map<? extends User, ?> m) {
+
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public Set<User> keySet() {
+                return null;
+            }
+
+            @Override
+            public Collection<Object> values() {
+                return null;
+            }
+
+            @Override
+            public Set<Entry<User, Object>> entrySet() {
+                return null;
+            }
+        };
+        map.put(user1, 1);
+        map.put(user2, 2);
+        System.out.println(map);
     }
 }
