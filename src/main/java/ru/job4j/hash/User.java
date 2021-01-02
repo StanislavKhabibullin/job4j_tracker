368b2d98a25e43afdebafeae072c50c1c5301c16
StanislavKhabibullin <66030700+StanislavKhabibullin@users.noreply.github.com>
2021-01-02 22:19:08 +0300

    2. Без переопределения equals и hashCode [#385714]

--- src/main/java/ru/job4j/hash/User.java
+++ src/main/java/ru/job4j/hash/User.java
@@ -34,77 +34,17 @@ public class User {
 
     @SuppressWarnings("checkstyle:EmptyLineSeparator")
     public static void main(String[] args) {
+        Map<User, Object> mappy = new HashMap<>();
         User user1 = new User();
         User user2 = new User();
         user1.setName("Fill");
         user1.setChildren(2);
         user1.setBirthday(new GregorianCalendar(1980, Calendar.JANUARY, 12));
-        user2.setName("Fillius");
-        user2.setChildren(1);
-        user2.setBirthday(new GregorianCalendar(1982, Calendar.MAY, 2));
-        Map<User, Object> map = new Map<User, Object>() {
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean containsKey(Object key) {
-                return false;
-            }
-
-            @Override
-            public boolean containsValue(Object value) {
-                return false;
-            }
-
-            @Override
-            public Object get(Object key) {
-                return null;
-            }
-
-            @Override
-            public Object put(User key, Object value) {
-                return null;
-            }
-
-            @Override
-            public Object remove(Object key) {
-                return null;
-            }
-
-            @Override
-            public void putAll(Map<? extends User, ?> m) {
-
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public Set<User> keySet() {
-                return null;
-            }
-
-            @Override
-            public Collection<Object> values() {
-                return null;
-            }
-
-            @Override
-            public Set<Entry<User, Object>> entrySet() {
-                return null;
-            }
-        };
-        map.put(user1, 1);
-        map.put(user2, 2);
-        System.out.println(map);
+        user2.setName("Fill");
+        user2.setChildren(2);
+        user2.setBirthday(new GregorianCalendar(1980, Calendar.JANUARY, 12));
+        mappy.put(user1, 1);
+        mappy.put(user2, 2);
+        System.out.println(mappy);
     }
 }
